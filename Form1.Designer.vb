a42d272f61085ae81bfc454a4703e702228054aa
Dell-User <Dell-User@DESKTOP-221INCO>
2024-01-17 17:04:08 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(56, 103)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(249, 19)
+        Label1.TabIndex = 0
+        Label1.Text = "Please enter the first number"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(56, 171)
+        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(272, 19)
+        Label2.TabIndex = 1
+        Label2.Text = "Please enter the second number"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Teal
+        Button1.Location = New Point(56, 239)
+        Button1.Margin = New Padding(4, 4, 4, 4)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(154, 101)
+        Button1.TabIndex = 2
+        Button1.Text = "Sum"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(363, 103)
+        TextBox1.Margin = New Padding(4, 4, 4, 4)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(141, 27)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(363, 163)
+        TextBox2.Margin = New Padding(4, 4, 4, 4)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(141, 27)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(363, 277)
+        TextBox3.Margin = New Padding(4, 4, 4, 4)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(141, 27)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 19F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Lime
+        ClientSize = New Size(1143, 570)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Font = New Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(4, 4, 4, 4)
+        Name = "Form1"
+        Text = "Adding Two Numbers"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Declaring Variables
+        Dim n As Integer
+
+        While n <> 5
+            n += 1
+
+            MsgBox(n)
+
+        End While
+        MsgBox("now out of the loop")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Declaring Variables
+        Dim firstNumber, SecondNumber As Integer
+        firstNumber = Val(TextBox1.Text)
+        SecondNumber = Val(TextBox2.Text)
+
+
+        Sum(firstNumber, SecondNumber)
+
+    End Sub
+    'The Sum Calcuation
+
+    Sub Sum(a As Integer, b As Integer)
+        'Declaring Variables
+        Dim total As Integer
+        total = a + b
+        TextBox3.Text = total
+
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+End Class
